241847b82178ce09eeac42a1a831db9473c1c623
keryan <keryan@macbook-pro-de-keryan.home>
2020-02-21 14:43:02 +0100

    ticket

--- app/src/components/casterLobby.vue
+++ app/src/components/casterLobby.vue
@@ -14,8 +14,11 @@
 
         <h3>{{users.length}} player<template v-if="users.length >> 1">s</template> in the party </h3>
 
-        <div :key="user.uid" v-for="user in this.users" class="user">
-            <span>{{ user }}</span>
+
+        <div :key="user.uid" v-for="user in this.users"  class="ticket">
+            <div class="ticket__content">
+                <p class="ticket__text">{{ user }}</p>
+            </div>
         </div>
     </div> 
 </template>
@@ -52,3 +55,59 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+$TICKET_COLOR: #005599;
+
+.ticket {
+  position: relative;
+  box-sizing: border-box;
+  width: 106px;
+  height: 150px;
+  margin: 50px auto 0;
+  padding: 10px;
+  border-radius: 8px;
+  background: $TICKET_COLOR;
+  box-shadow: 2px 2px 15px 0px #333;
+  
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    left: 5px;
+    height: 6px;
+    width: 96px;
+  }
+  
+  &:before {
+    top: -5px;
+    background: radial-gradient(circle, transparent, transparent 50%, $TICKET_COLOR 50%, $TICKET_COLOR 100% ) -7px -8px / 16px 16px repeat-x,
+  }
+  
+  &:after {
+    bottom: -5px;
+    background: radial-gradient(circle, transparent, transparent 50%, $TICKET_COLOR 50%, $TICKET_COLOR 100% ) -7px -2px / 16px 16px repeat-x,
+  }
+}
+
+.ticket__content {
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  border: 2px dotted #D8D8D8;
+}
+
+.ticket__text {
+    width: 100px;
+    max-width: 120%;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    font-size: 1.2rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    -webkit-transform: translate(-25px, 25px) rotate(-55deg);
+    transform: translate(-7px, 35px) rotate(-55deg);
+    word-break: break-all;
+    margin: 10px 0;
+    color: white;
+}
+</style>
